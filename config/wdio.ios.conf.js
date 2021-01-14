52e8c714a2d9d3412de22c31e0ee3485374e866f
cyrus.h <562792094@qq.com>
2021-01-14 11:02:09 +0800

    环境搭建

--- config/wdio.ios.conf.js
+++ config/wdio.ios.conf.js
@@ -131,7 +131,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec'],
+    // reporters: ['spec'],
 
 
     
@@ -264,6 +264,26 @@ exports.config = {
      * @param {<Object>} results object containing test results
      */
     // onComplete: function(exitCode, config, capabilities, results) {
+    // },
+    // onComplete: function() {
+    //     const reportError = new Error('Could not generate Allure report')
+    //     const generation = allure(['generate', 'allure-results', '--clean'])
+    //     return new Promise((resolve, reject) => {
+    //         const generationTimeout = setTimeout(
+    //             () => reject(reportError),
+    //             5000)
+
+    //         generation.on('exit', function(exitCode) {
+    //             clearTimeout(generationTimeout)
+
+    //             if (exitCode !== 0) {
+    //                 return reject(reportError)
+    //             }
+
+    //             console.log('Allure report successfully generated')
+    //             resolve()
+    //         })
+    //     })
     // },
     /**
     * Gets executed when a refresh happens.
--- test/specs/Test.App.yamimeal.js
+++ test/specs/Test.App.yamimeal.js
@@ -1,5 +1,9 @@
+const { addFeature } = require('@wdio/allure-reporter').default
+
 describe('case_01',() =>{
-    it('test',function(){
-        print(1==1)
+    
+    it('test_01',function(){
+        addFeature('frist test')
+        console.log(1 == 1)
     })
 })
