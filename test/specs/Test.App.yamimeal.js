6cec2e40e8bc855166243de298f7732327fb223a
cyrus.h <562792094@qq.com>
2021-01-26 10:22:41 +0800

    添加 手势api

--- test/specs/Test.App.yamimeal.js
+++ test/specs/Test.App.yamimeal.js
@@ -2,11 +2,24 @@ const AllureReporter = require('@wdio/allure-reporter').default
 const HomePage = require('../testiOSYamimeal/test_Home/home')
 const accountPage = require('../testiOSYamimeal/test_Home/account.element')
 const getAccountPage = require('../testiOSYamimeal/page_interface/accountPage')
+const Gestures = require('../../common/Gestures')
+// import Gestures from '../../common/Gestures'
 var assert = require('chai').assert;
 describe('case_01',() =>{
     it('test_01',function(){
         AllureReporter.addFeature('frist test')
         getAccountPage.inAccount();
+        driver.pause(1000)
+        try {
+            accountPage.waitIndicator(accountPage.activeincator)
+            
+        } catch (error) {
+            console.log(error)
+        }
+
+
+
+        
         var setTime = 6000
         driver.pause(setTime);
         var ss = new Array();
@@ -22,11 +35,25 @@ describe('case_01',() =>{
                 titleData.push(accountPage.title_01(i))
             }
         }
-        accountPage.clickTitle(titleData[3]);
         
-        driver.waitUntil(function () {
-            // console.log('为',accountPage.activeincator().isDisplayed())
-            return accountPage.activeIsTure() === false
-        },6000,'找不到该元素')
+        accountPage.clickTitle(titleData[3]);
+        driver.pause(2000)
+        // accountPage.waitIndicator(accountPage.activeincator)
+        try {
+            if (accountPage.waitIndicator(accountPage.activeincator)) {
+                console.log('元素为',accountPage.waitIndicator(accountPage.activeincator))
+            } 
+        } catch (e) {
+            console.log('跳出')
+            // break;
+            
+        }
+        driver.pause(2000)
+        const from = { x: 239, y:673 }
+        const to = { x: 245, y:394 } 
+        Gestures.swipe(from,to);
+        
+        
+        
     })
-}) 
+})
--- test/testiOSYamimeal/test_Home/account.element.js
+++ test/testiOSYamimeal/test_Home/account.element.js
@@ -4,6 +4,10 @@ class accountPage{
         
         return $(`-ios class chain:${selector}`);
     }
+    get scrollTouch(){
+        const selector = '**/XCUIElementTypeStaticText[`value == "测1"`][2]'
+        return $(`-ios class chain:${selector}`);
+    }
     collectionTitle(ele){
         const selector = `value == "${ele}"`
         return $(`-ios predicate string:${selector}`);
@@ -23,6 +27,10 @@ class accountPage{
     clickTitle(ele){
         this.collectionTitle(ele).click()
     }
+    moveToElement(element){
+        element.waitForDisplayer();
+        element.moveTo();
+    }
 
     clickCollectionItem(){
         this.clickCollectionItem.click()
@@ -33,10 +41,16 @@ class accountPage{
     title_01(index){
         console.log(this.aa(index).getAttribute("value"));
         return this.aa(index).getAttribute("value");
-        
-        
-        
     }
+    waitIndicator (element){
+       return driver.waitUntil(function () {
+           console.log('等待菊花',element.isDisplayed())
+            return element.isDisplayed() === true
+        },6000,'元素不存在')
+        
+
+    }
+    
     
 }
 
